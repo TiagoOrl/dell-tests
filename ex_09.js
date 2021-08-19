a4c4b2f729402d9d22bf51b12f734a6553717b0d
Tiago Orlando <tiago_orlando@yahoo.com>
2021-08-18 23:54:25 -0300

    2nd part of ex 09 completed

--- ex_09.js
+++ ex_09.js
@@ -1,6 +1,5 @@
 
 
-
 const categories = document.querySelectorAll("div.col-xs-4")
 let count = 0
 categories.forEach((i) => {
@@ -25,6 +24,7 @@ categories.forEach((i) => {
 
 })
 
+findAndChangeNetworkImgs()
 
 function makePurple() {
     const imgs = document.querySelectorAll("img.img-responsive")
@@ -45,4 +45,30 @@ function makeOrange() {
     imgs.forEach((i) => {
         i.style.background = "orange"
     })
+}
+
+
+function findAndChangeNetworkImgs(){
+    const prodImgs = document.querySelectorAll("div.half-inner-module-image")
+    let k = 0
+    prodImgs.forEach((i) => {
+        
+        if (i.getElementsByTagName("img")[0].alt.split(" ")[0].toLowerCase() === "tp-link"){
+            console.log("tp link at "+ k)
+            document.getElementsByClassName("half-inner-module-image")[k].getElementsByTagName("img")[0].src = 
+                document.getElementsByClassName("half-inner-module-image")[k + 25].getElementsByTagName("img")[0].src
+
+            document.getElementsByClassName("half-inner-module-image")[k].getElementsByTagName("img")[0].src = 
+            document.getElementsByClassName("half-inner-module-image")[k + 26].getElementsByTagName("img")[0].src
+
+            document.getElementsByClassName("half-inner-module-image")[k].getElementsByTagName("img")[0].src = 
+            document.getElementsByClassName("half-inner-module-image")[k + 27].getElementsByTagName("img")[0].src
+
+            document.getElementsByClassName("half-inner-module-image")[k].getElementsByTagName("img")[0].src = 
+            document.getElementsByClassName("half-inner-module-image")[k + 28].getElementsByTagName("img")[0].src
+        }
+
+        k++
+    })
+
 }
